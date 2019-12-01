6ebcdbbda4ddd60bd5693ae0336825c475b8c214
Jakub Kovář <jakub@kovar.co.uk>
2019-12-01 19:05:16 +0100

    Cavojsky webservice interface

--- app/src/main/java/com/example/messagingappmv/webservices/cavojsky/CavojskyWebServiceInterface.kt
+++ app/src/main/java/com/example/messagingappmv/webservices/cavojsky/CavojskyWebServiceInterface.kt
@@ -1,6 +1,42 @@
 package com.example.messagingappmv.webservices.cavojsky
 
+import com.example.messagingappmv.webservices.cavojsky.requestbodies.*
+import com.example.messagingappmv.webservices.cavojsky.responsebodies.*
+import retrofit.Call
+import retrofit.http.POST
+
 interface CavojskyWebServiceInterface {
 
+    // user endpoints
+    @POST("/user/create.php")
+    fun register(body: RegistrationRequest) : Call<RegistrationResponse>
 
+    @POST("/user/login.php")
+    fun login(body: LoginRequest) : Call<LoginResponse>
+
+    @POST("/user/refresh.php")
+    fun refreshTokens(body: RefreshRequest) : Call<RefreshResponse>
+
+    @POST("/user/fid.php")
+    fun setFirebaseId(body: UserFirebaseRequest) : Call<Void>
+
+    // room endpoints
+    @POST("/room/list.php")
+    fun getRooms(body: RoomListRequest) : Call<List<RoomListItem>>
+
+    @POST("/room/message.php")
+    fun sendMessageToRoom(body: RoomMessageRequest) : Call<Void>
+
+    @POST("/room/read.php")
+    fun getRoomMessages(body: RoomReadRequest) : Call<List<RoomReadItem>>
+
+    // contact endpoint
+    @POST("/contact/list.php")
+    fun getContacts(body: ContactListRequest) : Call<List<ContactListItem>>
+
+    @POST("/contact/message.php")
+    fun sendMessageToContact(body: ContactMessageRequest) : Call<Void>
+
+    @POST("/contact/read.php")
+    fun getContactMessages(body: ContactReadRequest) : Call<List<ContactReadItem>>
 }
