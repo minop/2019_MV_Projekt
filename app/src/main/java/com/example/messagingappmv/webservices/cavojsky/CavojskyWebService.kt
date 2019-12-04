46f006ffecf27c66ed9fe9a6243be69cd359ef97
Jakub Kovář <jakub@kovar.co.uk>
2019-12-04 12:34:08 +0100

    networking bugfixes

--- app/src/main/java/com/example/messagingappmv/webservices/cavojsky/CavojskyWebService.kt
+++ app/src/main/java/com/example/messagingappmv/webservices/cavojsky/CavojskyWebService.kt
@@ -2,16 +2,23 @@ package com.example.messagingappmv.webservices.cavojsky
 
 import android.content.Context
 import com.example.messagingappmv.webservices.cavojsky.interceptors.AuthInterceptor
+import com.example.messagingappmv.webservices.cavojsky.interceptors.LoginData
 import com.example.messagingappmv.webservices.cavojsky.interceptors.TokenAuthenticator
+import com.example.messagingappmv.webservices.cavojsky.interceptors.TokenStorage
+import com.example.messagingappmv.webservices.cavojsky.requestbodies.LoginRequest
 import com.example.messagingappmv.webservices.cavojsky.requestbodies.RefreshRequest
+import com.example.messagingappmv.webservices.cavojsky.requestbodies.RegistrationRequest
+import com.example.messagingappmv.webservices.cavojsky.requestbodies.RoomListRequest
 import com.example.messagingappmv.webservices.cavojsky.responsebodies.RefreshResponse
+import com.example.messagingappmv.webservices.cavojsky.responsebodies.RoomListItem
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.IllegalStateException
 
 object CavojskyWebService {
 
-    private const val BASE_URL = "zadanie.mpage.sk"
+    private const val BASE_URL = "http://zadanie.mpage.sk"
 
     private fun create(context: Context) : CavojskyWebServiceInterface {
         val httpClient = OkHttpClient.Builder()
@@ -22,6 +29,7 @@ object CavojskyWebService {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         return retrofit.create(CavojskyWebServiceInterface::class.java)
@@ -31,8 +39,42 @@ object CavojskyWebService {
         return IllegalStateException("webservice call failed: $error")
     }
 
+    // webservice operations
+    fun register(username: String, password: String, context: Context) {
+        val response = this.create(context).register(RegistrationRequest(username, password)).execute()
+
+        if(response.isSuccessful) {
+            val body = response.body()!!
+            TokenStorage.save(LoginData(body.uid, body.access, body.refresh), context)
+            return
+        }
+
+        throw makeException(response.errorBody()?.string()!!)
+    }
+
+    fun login(username: String, password: String, context: Context) {
+        val response = this.create(context).login(LoginRequest(username, password)).execute()
+
+        if(response.isSuccessful) {
+            val body = response.body()!!
+            TokenStorage.save(LoginData(body.uid, body.access, body.refresh), context)
+            return
+        }
+
+        throw makeException(response.errorBody()?.string()!!)
+    }
+
     fun refreshTokens(uid: String, refreshToken: String, context: Context) : RefreshResponse {
         val response = this.create(context).refreshTokens(RefreshRequest(uid, refreshToken)).execute()
+
+        if(response.isSuccessful)
+            return response.body()!!
+
+        throw makeException(response.errorBody()?.string()!!)
+    }
+
+    fun listRooms(context: Context) : List<RoomListItem> {
+        val response = this.create(context).getRooms(RoomListRequest( TokenStorage.load(context).uid )).execute()
 
         if(response.isSuccessful)
             return response.body()!!
--- app/src/main/java/com/example/messagingappmv/webservices/cavojsky/CavojskyWebServiceInterface.kt
+++ app/src/main/java/com/example/messagingappmv/webservices/cavojsky/CavojskyWebServiceInterface.kt
@@ -3,40 +3,41 @@ package com.example.messagingappmv.webservices.cavojsky
 import com.example.messagingappmv.webservices.cavojsky.requestbodies.*
 import com.example.messagingappmv.webservices.cavojsky.responsebodies.*
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface CavojskyWebServiceInterface {
 
     // user endpoints
     @POST("/user/create.php")
-    fun register(body: RegistrationRequest) : Call<RegistrationResponse>
+    fun register(@Body body: RegistrationRequest) : Call<RegistrationResponse>
 
     @POST("/user/login.php")
-    fun login(body: LoginRequest) : Call<LoginResponse>
+    fun login(@Body body: LoginRequest) : Call<LoginResponse>
 
     @POST("/user/refresh.php")
-    fun refreshTokens(body: RefreshRequest) : Call<RefreshResponse>
+    fun refreshTokens(@Body body: RefreshRequest) : Call<RefreshResponse>
 
     @POST("/user/fid.php")
-    fun setFirebaseId(body: UserFirebaseRequest) : Call<Void>
+    fun setFirebaseId(@Body body: UserFirebaseRequest) : Call<Void>
 
     // room endpoints
     @POST("/room/list.php")
-    fun getRooms(body: RoomListRequest) : Call<List<RoomListItem>>
+    fun getRooms(@Body body: RoomListRequest) : Call<List<RoomListItem>>
 
     @POST("/room/message.php")
-    fun sendMessageToRoom(body: RoomMessageRequest) : Call<Void>
+    fun sendMessageToRoom(@Body body: RoomMessageRequest) : Call<Void>
 
     @POST("/room/read.php")
-    fun getRoomMessages(body: RoomReadRequest) : Call<List<RoomReadItem>>
+    fun getRoomMessages(@Body body: RoomReadRequest) : Call<List<RoomReadItem>>
 
     // contact endpoint
     @POST("/contact/list.php")
-    fun getContacts(body: ContactListRequest) : Call<List<ContactListItem>>
+    fun getContacts(@Body body: ContactListRequest) : Call<List<ContactListItem>>
 
     @POST("/contact/message.php")
-    fun sendMessageToContact(body: ContactMessageRequest) : Call<Void>
+    fun sendMessageToContact(@Body body: ContactMessageRequest) : Call<Void>
 
     @POST("/contact/read.php")
-    fun getContactMessages(body: ContactReadRequest) : Call<List<ContactReadItem>>
+    fun getContactMessages(@Body body: ContactReadRequest) : Call<List<ContactReadItem>>
 }
