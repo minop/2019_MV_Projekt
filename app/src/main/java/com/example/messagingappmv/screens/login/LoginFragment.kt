a9f0ca621be5082369fb716dfe83a0e775530569
Jakub Kovář <jakub@kovar.co.uk>
2019-12-09 18:30:59 +0100

    initialized login fragment

--- app/src/main/java/com/example/messagingappmv/screens/login/LoginFragment.kt
+++ app/src/main/java/com/example/messagingappmv/screens/login/LoginFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.messagingappmv.R
+import com.example.messagingappmv.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding : FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/messagingappmv/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/messagingappmv/screens/login/LoginViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.messagingappmv.screens.login
 
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+
+}
