146112f7589f7e78e40fc22b11dca8a45df2925c
Jakub Kovář <jakub@kovar.co.uk>
2019-12-09 18:46:25 +0100

    LoginViewModel

--- app/src/main/java/com/example/messagingappmv/screens/login/LoginFragment.kt
+++ app/src/main/java/com/example/messagingappmv/screens/login/LoginFragment.kt
@@ -6,12 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.messagingappmv.R
 import com.example.messagingappmv.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    private lateinit var viewModel: LoginViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding : FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
 
         return binding.root
     }
--- app/src/main/java/com/example/messagingappmv/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/messagingappmv/screens/login/LoginViewModel.kt
@@ -3,5 +3,10 @@ package com.example.messagingappmv.screens.login
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+    var username = ""
+    var password = ""
 
+    init {
+
+    }
 }
